8dd64b337d6ee2d69154768f75e332307f9f3e7d
shilpa4u <shilpanie@gmail.com>
2022-10-17 17:37:07 +0900

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">7Streams</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
